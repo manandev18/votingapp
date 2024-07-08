d6134619c687b02e84dfa94f86bbbe26a7b91b33
manandev18 <andraskarmanan725@gmail.com>
2024-07-08 20:13:45 +0530

    jwt modified

--- jwt.js
+++ jwt.js
@@ -11,7 +11,7 @@ const jwtAuthMiddleware = (req, res, next) => {
 
   try {
     // Verify the JWT token
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, `${process.env.JWT_SECRET}`);
 
     // Attach user information to the request object
     req.user = decoded;
@@ -25,7 +25,7 @@ const jwtAuthMiddleware = (req, res, next) => {
 // Function to generate JWT token
 const generateToken = (userData) => {
   // Generate a new JWT token using user data
-  return jwt.sign(userData, process.env.JWT_SECRET, { expiresIn: 30000 });
+  return jwt.sign(userData, `${process.env.JWT_SECRET}`, { expiresIn: 30000 });
 };
 
 module.exports = { jwtAuthMiddleware, generateToken };
